c842f80ab0d3c9a99fdee80449faf7130cd400ac
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-05-08 12:45:59 +0300

    [add] new layout for flow details

--- game-interface-flows-project/src/components/FlowDetails.tsx
+++ game-interface-flows-project/src/components/FlowDetails.tsx
@@ -2,25 +2,42 @@ import React from "react";
 
 import { IFlow } from "../models/flow";
 
-import { Comment } from "./Comment";
-
 interface FlowDetailsProps {
     flow: IFlow | null;
 }
 
 export function FlowDetails({ flow }: FlowDetailsProps) {
     return (
-        <div className="container-fluid bg-dark py-3">
-            <h2 className="text-white text-uppercase">{flow?.title}</h2>
-            <p className="text-white">{flow?.description}</p>
-            <h2 className="text-white text-uppercase">comments</h2>
-            <div className="row d-flex justify-content-center">
-                <div className="col-12">
-                    {flow?.comments.map((comment) => (
-                        <Comment key={comment.id} comment={comment} />
-                    ))}
-                </div>
+        <div className="container-fluid bg-dark text-white py-3">
+            <h1 className="text-uppercase mb-0">{flow?.title}</h1>
+            <div className="mb-2">
+                {flow?.platforms.map((platform) => (
+                    <span
+                        key={platform.id}
+                        className="badge bg-light text-dark me-1 text-uppercase"
+                    >
+                        {platform.name}
+                    </span>
+                ))}
+                {flow?.genres.map((genre) => (
+                    <span
+                        key={genre.id}
+                        className="badge bg-light text-dark me-1 text-uppercase"
+                    >
+                        {genre.name}
+                    </span>
+                ))}
             </div>
+            <p>
+                by {flow?.author.username}, {flow?.date}
+            </p>
+            {flow?.source ? (
+                <a href={flow.source}>{flow.source}</a>
+            ) : (
+                <p>No source</p>
+            )}
+            <p>Total Screens: 0</p>
+            <p>Average Connectivity: 0</p>
         </div>
     );
 }
