659b63af47438166af74a5b83290881730bc0157
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-05-08 12:45:32 +0300

    [fix] models

--- game-interface-flows-project/src/models/flow.ts
+++ game-interface-flows-project/src/models/flow.ts
@@ -1,12 +1,19 @@
 import { IComment } from "./comment";
+import { IGenre } from "./genre";
+import { IPlatform } from "./platform";
 import { IScreen } from "./screen";
 import { IScreenSharableProperties } from "./screen_properties";
+import { IUser } from "./user";
 
 export interface IFlow {
     id: number;
     title: string;
-    description: string;
+    source: string;
+    date: string;
     screens: [IScreen];
     screens_properties: IScreenSharableProperties;
     comments: [IComment];
+    author: IUser;
+    genres: [IGenre];
+    platforms: [IPlatform];
 }
--- game-interface-flows-project/src/models/flow_preview.ts
+++ game-interface-flows-project/src/models/flow_preview.ts
@@ -4,7 +4,6 @@ import { IPlatform } from "./platform";
 export interface IFlowPreview {
     id: number;
     title: string;
-    description: string;
     date: string;
     total_likes: number;
     flow_thumbnail_url: string;
