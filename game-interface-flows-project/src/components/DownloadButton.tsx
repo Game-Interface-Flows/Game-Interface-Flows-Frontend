1648ade1062f1cb32241acbc199a7d3159be24da
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-05-11 19:42:50 +0300

    [add] download images button

---
+++ game-interface-flows-project/src/components/DownloadButton.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
+import { IScreen } from "../models/screen";
+
+interface DownloadProps {
+    title: string;
+    screens: IScreen[];
+}
+
+const DownloadButton: React.FC<DownloadProps> = ({ title, screens }) => {
+    const [isDownloading, setIsDownloading] = useState(false);
+
+    const downloadImagesAsZip = async (screens: IScreen[]) => {
+        const imageUrls = screens.map((screen) => screen.image);
+        const folderName = `images_${title.replace(/\s/g, "_")}`;
+
+        setIsDownloading(true);
+
+        const zip = new JSZip();
+        const imgFolder = zip.folder(folderName);
+
+        if (!imgFolder) {
+            console.error("Failed to create a folder in the zip file");
+            return;
+        }
+
+        const promises = imageUrls.map(async (url, index) => {
+            const response = await fetch(url);
+            const blob = await response.blob();
+            imgFolder.file(`image${index + 1}.png`, blob);
+        });
+
+        await Promise.all(promises);
+
+        zip.generateAsync({ type: "blob" })
+            .then((content) => {
+                saveAs(content, `${folderName}.zip`);
+                setIsDownloading(false);
+            })
+            .catch((error) => {
+                console.error("Failed to generate zip file:", error);
+                setIsDownloading(false);
+            });
+    };
+
+    return (
+        <div>
+            <button
+                className={"btn w-100 btn-primary text-uppercase"}
+                disabled={isDownloading}
+                onClick={() => downloadImagesAsZip(screens)}
+            >
+                {isDownloading ? (
+                    <span
+                        className="spinner-border spinner-border-sm"
+                        role="status"
+                        aria-hidden="true"
+                    ></span>
+                ) : (
+                    "Download Images"
+                )}
+            </button>
+        </div>
+    );
+};
+
+export default DownloadButton;
--- game-interface-flows-project/src/components/FlowDetails.tsx
+++ game-interface-flows-project/src/components/FlowDetails.tsx
@@ -2,19 +2,20 @@ import React from "react";
 
 import { IFlow } from "../models/flow";
 import { LikeButton } from "./LikeButton";
+import DownloadButton from "./DownloadButton";
 
 interface FlowDetailsProps {
-    flow: IFlow | null;
+    flow: IFlow;
 }
 
 export function FlowDetails({ flow }: FlowDetailsProps) {
     return (
         <div className="container-fluid bg-dark text-white py-3">
             <h2 className="text-uppercase mb-0">
-                {flow?.title} ({flow?.id})
+                {flow.title} ({flow.id})
             </h2>
             <div className="mb-2">
-                {flow?.platforms.map((platform) => (
+                {flow.platforms.map((platform) => (
                     <span
                         key={platform.id}
                         className="badge bg-light text-dark me-1 text-uppercase"
@@ -22,7 +23,7 @@ export function FlowDetails({ flow }: FlowDetailsProps) {
                         {platform.name}
                     </span>
                 ))}
-                {flow?.genres.map((genre) => (
+                {flow.genres.map((genre) => (
                     <span
                         key={genre.id}
                         className="badge bg-light text-dark me-1 text-uppercase"
@@ -32,17 +33,17 @@ export function FlowDetails({ flow }: FlowDetailsProps) {
                 ))}
             </div>
             <p>
-                by {flow?.author.username}, {flow?.date}
+                by {flow.author.username}, {flow.date}
             </p>
             <div className="mb-4">
                 <h3 className="text-uppercase">Details</h3>
-                {flow?.source ? (
+                {flow.source ? (
                     <a href={flow.source}>{flow.source}</a>
                 ) : (
                     <p>No source</p>
                 )}
-                <p>Total Screens: {flow?.total_screens}</p>
-                <p>Average Connectivity: {flow?.average_connectivity}</p>
+                <p>Total Screens: {flow.total_screens}</p>
+                <p>Average Connectivity: {flow.average_connectivity}</p>
                 {flow && <LikeButton flow={flow} />}
             </div>
             <div>
@@ -51,6 +52,7 @@ export function FlowDetails({ flow }: FlowDetailsProps) {
                     You cannot edit a flow here but soon a plugin for Figma will
                     be released.
                 </p>
+                <DownloadButton title={flow.title} screens={flow.screens} />
             </div>
         </div>
     );
